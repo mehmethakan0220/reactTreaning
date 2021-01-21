d00c679402471512adf20406651321acb3e4817a
Archerno <mehmethakan0220@gmail.com>
2021-01-21 12:11:09 +0300

    react elementine css uygulamak

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
 const user = {
-  firstname:"john",
-  lastname:"doe"
+  firstname: "john",
+  lastname: "doe"
 };
 
 function getFullname(user) {
   return user.firstname + " " + user.lastname;
 }
 
-
 const element = <h1> hello {getFullname(user)} , welcome </h1>;
 
 
+//boş etiket tanımlama jsx e ait bir özellik değildir html e aittir örneğin <br/> etiketi
+
+// aynı şekilde attribute ekleyebiliriz.
+
+const jsxElement = <h1 style={{color:"blue", textAlign:"center"}} />
+// bu stilleri inspect ederek görüntüleyebiliriz.
+
+
+
+
+ReactDOM.render(jsxElement, document.getElementById("root"));
 
 
 
@@ -32,7 +42,42 @@ const element = <h1> hello {getFullname(user)} , welcome </h1>;
 
 
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
